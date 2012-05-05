949597c7d0505b3cbe51a5257ecae6642610981a
Matthieu Moquet <matthieu@moquet.net>
2012-05-05 16:01:26 +0200

    Initial commit

---
+++ assets/config.rb
@@ -0,0 +1,11 @@
+# Set this to the root of your project when deployed:
+http_path       = "/assets/"
+css_dir         = "css"
+sass_dir        = "scss"
+images_dir      = "images"
+fonts_dir       = "fonts"
+javascripts_dir = "js"
+
+# Extra configuration
+line_comments = false
+#relative_assets = true
